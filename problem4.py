641ccbb344ecf5156cc947b2505c377c0c83ac17
Colt Frederickson <coltfred@gmail.com>
2011-05-09 14:47:07 -0600

    I don't know why, but git just committed an old version...

--- problem4.py
+++ problem4.py
@@ -1,21 +1,21 @@
 def is_pal(s):
     length = len(s)
     half = length/2
-    #print " checking %s against %s" % (s[0:-half],s[half:length][::-1])
     return s[0:-half] == s[half:length][::-1]
 
 if __name__ == '__main__':
-    i = 1000
-    j = 1000
-    ans = None
-    while not ans and i > 0:
-        i += -1
-        while not ans and j > 0:
+    i = 999
+    j = 999
+    ans = 0
+    while i > 0:
+        while j > 0:
+            temp = i*j
+            if is_pal(str(temp)):
+                if temp > ans:
+                    ans = temp
+                else:
+                    break;
             j += -1
-            temp = str(i*j)
-            if is_pal(temp):
-                ans = temp
-                print "i was %d and j was %d:%s" % (i,j,ans)
-
-        j = 1000
-
+        i += -1
+        j = 999
+    print ans
