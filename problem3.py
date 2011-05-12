ab0954abe155c64777bcccea5df092e89a80f5bf
Colt Frederickson <coltfred@gmail.com>
2011-05-12 16:24:19 -0600

    add 1 to sqrt

--- problem3.py
+++ problem3.py
@@ -2,22 +2,22 @@ import math
 '''What is the largest prime factor of the number 600851475143 ?'''
 def IsPrime( n ): 
     if n == 2: 
-        return 1 
+        return True 
     elif n % 2 == 0: 
-        return 0 
+        return False 
 
     divisor = 3 
     range = math.ceil(math.sqrt(n))
     while( divisor < range ): 
         if( n % divisor == 0): 
-            return 0 
+            return False 
         divisor += 1 
-    return 1 
+    return True 
 
     
 if __name__ == "__main__":
     num = 600851475143
-    next_check = math.ceil(math.sqrt(num))
+    next_check = math.ceil(math.sqrt(num)) + 1
     
     while True: 
         if num % next_check == 0 and IsPrime(next_check): 
