8383024ee20196b20513c853175ed35053655ed4
Colt Frederickson <coltfred@gmail.com>
2011-05-26 20:32:23 -0600

    1 line solution using conversions and maps.

---
+++ problem48.py
@@ -0,0 +1,9 @@
+'''
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+'''
+
+if __name__ == "__main__":
+    print str(sum([int(str(i**i)[-10::]) for i in range(1,1000)]))[-10::]
+    
